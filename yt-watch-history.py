a7aaac67341adc39a13d04ff61a9dd2bacdf269b
Tobias Prochazka <protob@seznam.cz>
2023-08-03 23:42:54 +0200

    Update yt-watch-history.py

--- yt-watch-history.py
+++ yt-watch-history.py
@@ -1,5 +1,5 @@
 import json
- 
+#ENG VERSION = json file in english 
 f = open("watch-history.json", encoding="utf8")
  
 data = json.load(f)
@@ -8,29 +8,26 @@ pruned = {}
 music = True
 for entry in data:
     if "subtitles" in entry:
-        #ENG VERSION (entry["title"] != "Visited YouTube Music")
-        #entry["header"] == "YouTube" pro norm. videa "YouTube Music" pro ytmusic
+
+        #FOR ALL YOUTUBE VIDS entry["header"] == "YouTube" 
         if ((entry["header"] == "YouTube Music") == music) and (entry["title"] != "Zhlédnutí videa, které bylo odstraněno") and ("https://www.youtube.com/watch?v=" not in entry["title"]) and (entry["title"] != "Watched a video that has been removed"):
-            
+        #ENG VERSION (entry["title"] != "Visited YouTube Music")
             title = entry["title"][8::] 
-            time = entry["time"]  
 
             if music:
                 artist  = entry["subtitles"][0]["name"].replace(" - Topic", "")
                 title = f'{title} - {artist}'.replace("jste video ","")
-                #, last watched: {time}
+                #ENG VERSION no need for .replace("jste video ","")
     
             if title in pruned:
                 pruned[title] = pruned[title] + 1
-                pruned[time] = time
             else:
                 pruned[title] = 1
-                pruned[time] = time
 
 total_plays = 0
  
 for song, listens in dict(sorted(pruned.items(), key=lambda item: item[1])).items():
     total_plays += listens
-    print(f'{song} [times listened: {listens}] [last watched: {time}]')
+    print(f'{song} [times listened: {listens}]')
  
 print(f'{total_plays} total plays across all tracks')
